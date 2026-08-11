6649bde2f4c956f724c9b5c05029138dbd8a6e05
luke <luke.mbindyo@outlook.com>
2026-08-11 19:20:17 -0400

    ai situation generation completed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,36 @@
-require('dotenv').config()
-import express from "express"
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json())
+app.use(cors())
 
-app.post('/getAIR', async (req, res)) =>  {
+app.post('/getAIR', async (req, res) => {
    
-}
+  const aiData = await fetch('https://api.anthropic.com/v1/messages', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'x-api-key': process.env.API_KEY,
+      'anthropic-version': "2023-06-01",
+    },
+
+    body: JSON.stringify({
+      model: 'claude-haiku-4-5',
+      max_tokens: 1024,
+      messages: [{role: 'user', content: req.body.message}],
+    }),
+  });
+
+  const aiSituation = await aiData.json();
+ 
+  res.json(aiSituation);
+
+})
+
+app.listen(PORT, () => {
+  console.log('server is running');
+} )
--- training.js
+++ training.js
@@ -16,15 +16,33 @@ userResponse.addEventListener("input", () => {
 })
 
 async function getChallenge() {
+
+  const message = `I have an application called overmind. It tests users cognitive abilities and helps them foster their decision making. You are the mentor that will give them a situation according to a topic and tell them how they did with scrutiny. Now please give them a situation that cab be applicable in the real world and actually have them interested and wanting to do more challenges. It has to mae them learn, be fun, and addictive. The topic is ${topic}. Make the response around 270 characters. can go less or more if you want. Remember only the challenge not the scoring yet. Try to be unique with the situations so we don't get the same one twice`;
+
+  const messageObj = {
+    message: message
+  }
   
-  const sendAIData = await fetch("", {
-    
-  })
+  const sendAIData = await fetch("http://localhost:3000/getAIR", {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+
+    body: JSON.stringify(messageObj)
+  }) 
+  
+  const aiSituation = await sendAIData.json();
+
+  console.log(aiSituation);
+  console.log('hello');
 }
 
 async function sendResponse(userResponse) {
   
 }
+
+ getChallenge();
 
 submitButton.addEventListener('click', () => {
   sendResponse(userResponse.value)
